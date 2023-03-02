a2eb563095b8164f31d59ce9ff10554591c334cd
witold1885 <wiktor8555@gmail.com>
2023-03-02 18:25:30 +0200

    getCategoryProducts route

--- app/Http/Controllers/Api/CatalogController.php
+++ app/Http/Controllers/Api/CatalogController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\ProductCategory;
+use App\Models\Product;
 
 class CatalogController extends Controller
 {
