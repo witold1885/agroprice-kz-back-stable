289d82c1236ae8d9a64b1117cd80b8872b7ac163
witold1885 <wiktor8555@gmail.com>
2023-03-25 21:52:06 +0200

    get profile products

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\UserProfile;
+use App\Models\Product;
 
 class ProfileController extends Controller
 {
