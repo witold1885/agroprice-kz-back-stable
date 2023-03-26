451c95ea44bae7afbed6da68cf42314b8f00a507
witold1885 <wiktor8555@gmail.com>
2023-03-26 17:02:34 +0300

    Get profile products - add categories, location, images

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -97,16 +97,17 @@ class ProfileController extends Controller
         try {
             $product = Product::where('id', $product_id)->with('location')->with('productImages')->first();
 
-            /*$product->categories = [];
-            $product_categories = ProductCategory::where('product_id', $product->id)->get();
-            foreach ($product_categories as $product_category) {
-                $category = Category::find($product_category->category_id);
-                if ($category) $product->categories[] = $category;
-            }*/
-
             if (!$product) {
                 return response()->json(['success' => false, 'error' => 'Объявление не найдено']);
             }
+
+            $categories = [];
+            $product_categories = ProductCategory::where('product_id', $product->id)->get();
+            foreach ($product_categories as $product_category) {
+                $category = Category::find($product_category->category_id);
+                if ($category) $categories[] = $category;
+            }
+            $product->categories = $categories;
 
             return response()->json(['success' => true, 'product' => $product]);
         } catch (\ErrorException $e) {
