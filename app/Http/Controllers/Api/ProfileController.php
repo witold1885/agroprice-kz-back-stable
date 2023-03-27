759e7dddfa3d94b2d20af0b1f4ca300e4358399a
witold1885 <wiktor8555@gmail.com>
2023-03-27 10:06:39 +0300

    Test profile products with pagination

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -56,17 +56,17 @@ class ProfileController extends Controller
 
             if (!$status) {
                 $products = Product::/*where('user_id', $user_id)->*/with('location')->with('productImages')->skip($offset)->take($limit)->get();
-                $total = Product::where('user_id', $user_id)->count();
+                $total = Product::/*where('user_id', $user_id)->*/count();
             }
             elseif ($status == 'published') {
                 $products = Product::/*where('user_id', $user_id)->*/whereIn('status', ['published', 'accepted'])
                     ->with('location')->with('productImages')->skip($offset)->take($limit)->get();
-                $total = Product::where('user_id', $user_id)->whereIn('status', ['published', 'accepted'])->count();
+                $total = Product::/*where('user_id', $user_id)->*/whereIn('status', ['published', 'accepted'])->count();
             }
             else {
                 $products = Product::/*where('user_id', $user_id)->*/where('status', $status)
                     ->with('location')->with('productImages')->skip($offset)->take($limit)->get();
-                $total = Product::where('user_id', $user_id)->where('status', $status)->count();
+                $total = Product::/*where('user_id', $user_id)->*/where('status', $status)->count();
             }
 
             $pages = ceil($total / $limit);
