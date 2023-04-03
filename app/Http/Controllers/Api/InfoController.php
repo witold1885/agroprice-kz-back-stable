bc541766a2b506e672f7862e3f195ec539b5351c
witold1885 <wiktor8555@gmail.com>
2023-04-03 17:35:19 +0300

    Get last blog articles

--- app/Http/Controllers/Api/InfoController.php
+++ app/Http/Controllers/Api/InfoController.php
@@ -62,10 +62,21 @@ class InfoController extends Controller
         }
     }
 
+    public function getLastBlogArticles()
+    {
+        try {
+            $lastArticles = Article::where('type', 'blog')->orderBy('date', 'desc')->limit(10)->get();
+
+            return response()->json(['success' => true, 'lastArticles' => $lastArticles]);
+        } catch (\ErrorException $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()]);
+        }
+    }
+
     public function getBlogArticles(Request $request)
     {
         try {
-            $limit = 12;
+            $limit = 20;
             $offset = ($request->page - 1) * $limit;
 
             $query = Article::where('type', 'blog');
@@ -77,7 +88,7 @@ class InfoController extends Controller
             $total = $query->count();
             $articles = $query->skip($offset)->take($limit)->get();
 
-            $lastArticles = Article::where('type', 'blog')->orderBy('date', 'desc')->limit(4)->get();
+            $lastArticles = Article::where('type', 'blog')->orderBy('date', 'desc')->limit(5)->get();
 
             return response()->json(['success' => true, 'articles' => $articles, 'lastArticles' => $lastArticles, 'total' => $total]);
         } catch (\ErrorException $e) {
--- routes/api.php
+++ routes/api.php
@@ -84,6 +84,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::group(['prefix' => 'blog'], function () {
         // Route::get('banners/get', [LocationController::class, 'getBanners']);
         Route::post('articles', [InfoController::class, 'getBlogArticles']);
+        Route::get('last-articles', [InfoController::class, 'getLastBlogArticles']);
         Route::get('article/{url}', [InfoController::class, 'getBlogArticle']);
         Route::post('increase-views', [InfoController::class, 'increaseArticleViews']);
     });
